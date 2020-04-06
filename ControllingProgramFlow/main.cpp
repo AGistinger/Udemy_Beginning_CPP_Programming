9c8a89139ba8414461a6f7be82d995437658222c
Adrianne <54363707+AGistinger@users.noreply.github.com>
2020-04-06 14:31:32 -0400

    Add files via upload

---
+++ ControllingProgramFlow/main.cpp
@@ -0,0 +1,34 @@
+// Controlling Program Flow
+/*
+ * Sequence - Each statement followed by each other
+ * Selection - Making decisions execute parts of program when conditions are true or false
+ * Iteration - Looping, lets you repeat parts of a program
+ * 
+ * if statement - if condition is true you do something
+ * if-else - If a condition is true you do something, if it is false you do something else
+ * Nested if statements - If fav football team playing today, if game is on now, then record it
+ * Switch statment - Comparing integer based elements
+ * Conditional operator - ?: if/else wrapped in an operator
+ * 
+ * Iteration - Looping
+ * for loop
+ * range-based for loop - Works for arrays/vectors of items
+ * while loop
+ * do-while loop
+ * continue and break
+ * infinite loops - loops forever
+ * nested loops - loops within loops
+ * 
+ * */
+ 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
